6f42742d6512b87fd0233a7c62a0a56d94cfaa57
rosen1000 <rosen.ruseff@gmail.com>
2019-10-14 17:49:46 +0300

    added main and made fetch-gifs

--- fetch-gif.js
+++ fetch-gif.js
@@ -1,6 +1,8 @@
 function get(giphy) {
     return new Promise((res, rej) => {
-        
+        giphy.search("trippy").then(gif => {
+            res(gif);
+        });
     });
 }
 
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const giphy = require("giphy-api")(process.env.KEY);
 const fetch_gif = require("./fetch-gif");
 
-let gif;
-fetch_gif(giphy).then(g => gif = g).catch(e => console.log(e));
+let main = async function () {
+    let gif;
+    await fetch_gif(giphy).then(g => gif = g).catch(e => console.log(e));
+    console.log(gif);
+}
 
-let thing = require("./code");
-console.log(thing);
+main();
