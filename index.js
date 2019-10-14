21bb69bf112994a122f2799a11cb1213e738d99a
rosen1000 <rosen.ruseff@gmail.com>
2019-10-15 00:29:31 +0300

    interval for downloading

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const cache_mng = require("./cache-mng");
 
 let main = async function () {
     let gif;
-    await fetch_gif(giphy).then(g => gif = g).catch(e => console.log(e));
-    await store_gif(gif.data[Math.floor(Math.random() * gif.data.length)].images["original"].url);
+    setInterval(async () => {
+        for (let i = 0; i < 10; i++) {
+            await fetch_gif(giphy).then(g => gif = g).catch(e => console.log(e));
+            await store_gif(gif.data[Math.floor(Math.random() * gif.data.length)].images["original"].url);
+        }
+    }, 60000);
     await cache_mng.set();
 }
 
