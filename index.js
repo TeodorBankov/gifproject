14a426719ef90bac150cab755ac1e82c3e7936e1
TeodorBankov <teodor2003.tp@gmail.com>
2019-10-14 23:41:22 -0400

    deletion of caching gifs and some debbuging

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let main = async function () {
     let gif;
     await fetch_gif(giphy).then(g => gif = g).catch(e => console.log(e));
     await store_gif(gif.data[Math.floor(Math.random() * gif.data.length)].images["original_still"].url);
+    
 }
 
 main();
