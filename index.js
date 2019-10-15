3473c6a6200cfd1cace3a17d8d20d95d10f927ac
rosen1000 <rosen.ruseff@gmail.com>
2019-10-15 17:23:19 +0300

    added config

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const giphy = require("giphy-api")(process.env.KEY);
 const fetch_gif = require("./fetch-gif");
 const store_gif = require("./storegifs");
 const cache_mng = require("./cache-mng");
+const fs = require("fs");
+
+let rawdata = fs.readFileSync("config.json");
+let config = JSON.parse(rawdata);
 
 let main = async function () {
     let gif;
@@ -27,10 +31,20 @@ process.stdin.setEncoding("utf8");
 
 process.stdin.on("data", async chunk => {
     let lines = chunk.split("\n");
-    if (lines[0] == "exit") {
-        process.exit();
-    } 
-})
+    let args = lines[0].split(" ");
 
+    if (args[0] == "exit") {
+        process.exit();
+    } else if (args[0] == "config") {
+        switch (args[1]) {
+            case "search":
+                config.search = args.slice(2).join(" ") || "Trippy";
+                fs.writeFileSync("config.json", JSON.stringify(config));
+                break;
+            default:
+                console.log("Missing arguments");
+        }
+    }
+})
 
 main();
