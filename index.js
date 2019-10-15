8924e717883c300ae581958d2fb6a56d581c95fe
TeodorBankov <teodor2003.tp@gmail.com>
2019-10-15 00:49:06 -0400

    added function to download 10 gifs in a 10 sec interval

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ const cache_mng = require("./cache-mng");
 
 let main = async function () {
     let gif;
+    var timer = 1;
     setInterval(async () => {
+       timer++; 
+       console.log(timer);
+    }, 1000);
+    setInterval(async () => {
+    if(timer%10==0){
         for (let i = 0; i < 10; i++) {
-            await fetch_gif(giphy).then(g => gif = g).catch(e => console.log(e));
-            await store_gif(gif.data[Math.floor(Math.random() * gif.data.length)].images["original"].url);
-        }
-    }, 60000);
+        await fetch_gif(giphy).then(g => gif = g).catch(e => console.log(e));
+        await store_gif(gif.data[Math.floor(Math.random() * gif.data.length)].images["original"].url);
+          }
+    timer = 1;
+    }
     await cache_mng.set();
+    await cache_mng.clear();
+    }, 1000);   
 }
-
+//needs a function update.
 main();
