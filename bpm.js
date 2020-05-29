9acfed78fcf2c1e9b35d6f382e419a6c69e4fbda
TeodorBankov <teodor2003.tp@gmail.com>
2020-05-29 15:27:58 +0300

    added bpm detection

--- bpm.js
+++ bpm.js
@@ -1,1 +1,27 @@
-/*GAY JS*/
+var AudioContext = require("web-audio-api").AudioContext;
+var MusicTempo = require("music-tempo");
+var fs = require("fs");
+ 
+var calcTempo = function (buffer) {
+  var audioData = [];
+  // Take the average of the two channels
+  if (buffer.numberOfChannels == 2) {
+    var channel1Data = buffer.getChannelData(0);
+    var channel2Data = buffer.getChannelData(1);
+    var length = channel1Data.length;
+    for (var i = 0; i < length; i++) {
+      audioData[i] = (channel1Data[i] + channel2Data[i]) / 2;
+    }
+  } else {
+    audioData = buffer.getChannelData(0);
+  }
+  var mt = new MusicTempo(audioData);
+ 
+  console.log(mt.tempo);
+  //console.log(mt.beats);
+}
+ 
+var data = fs.readFileSync("./cached-songs/czTksCF6X8Y.flac");
+ 
+var context = new AudioContext();
+context.decodeAudioData(data, calcTempo);
